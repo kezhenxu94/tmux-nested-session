154a3320fa1a1e07db5f9aa7765bf1acbeb56ce7
kezhenxu94 <kezhenxu94@apache.org>
2023-09-27 08:27:04 +0000

    Send key to remote when toggling local

--- scripts/disable_tmux.sh
+++ scripts/disable_tmux.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-if tmux list-clients > /dev/null 2>&1; then
-  tmux set -g prefix None
-  tmux set -g key-table off
-  tmux set -g status off
-  tmux refresh-client -S
-fi
+tmux set -g prefix None
+tmux set -g key-table off
+tmux set -g status off
+tmux refresh-client -S
